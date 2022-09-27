6f9dcb4d4420e9e231ed2a2e4e8218d057cadfd3
Hanuman Prasad Meena <hanuallrounder@gmail.com>
2022-09-27 17:04:41 +0530

    pwa added

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
-const API_KEY = 'enter_API_key';
+const API_KEY = "enter your api key";
 export const fetchWeather = async (query) => {
   const { data } = await axios.get(URL, {
     params: {
